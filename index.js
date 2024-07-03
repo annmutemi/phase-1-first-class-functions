a7b32a8bbee5c346d3caa0d95345796add23c33e
annmutemi <annshiru92@gmail.com>
2024-07-03 15:19:48 +0300

    returning a function

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(bikeRide){
+bikeRide()
+}
+
+function bikeRide(){
+    return "ride my pace bike"
+}
+
+function returnsANamedFunction(){
+    return function ann () {
+        console.log("returning functions")
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return functioncls (){
+        return ""
+    }
+}
